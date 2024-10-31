fc3d4cbf7daa223808f03594d72235dd50bc6e04
Cass <89551610+cealcorn@users.noreply.github.com>
2024-10-31 14:21:33 +0000

    fixed main require path in test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var main = require('../index');
-var asset = require('assert');
+var main = require('/workspaces/devops/index.js');
+var assert = require('assert');
 
 describe('First two terms', function() {
     it('should return 1 for both 0 and 1', function() {
-        asset.strictEqual(main.factNum(0), 1);
-        asset.strictEqual(main.factNum(1), 1);
+        assert.strictEqual(main.factNum(0), 1);
+        assert.strictEqual(main.factNum(1), 1);
     });
 });
